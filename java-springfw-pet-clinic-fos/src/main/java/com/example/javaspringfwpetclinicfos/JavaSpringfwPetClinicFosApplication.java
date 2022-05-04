9c04ffdce2f090381d556a0acc1d464b623f2b57
adminich <12345>
2022-05-04 19:37:13 +0200

    Initial project add

---
+++ java-springfw-pet-clinic-fos/src/main/java/com/example/javaspringfwpetclinicfos/JavaSpringfwPetClinicFosApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringfwpetclinicfos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringfwPetClinicFosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringfwPetClinicFosApplication.class, args);
+    }
+
+}
---
+++ java-springfw-pet-clinic-fos/src/test/java/com/example/javaspringfwpetclinicfos/JavaSpringfwPetClinicFosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringfwpetclinicfos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringfwPetClinicFosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
